4dff9e6e8e1f3d386d9e29d5a83c754016fb1a08
MatheusVenosa <matheus.venosa@usp.br>
2018-08-22 13:19:39 -0300

    first commit

---
+++ mvenosalib2/src/main/java/com/example/matheus/mvenosalib/Utils.kt
@@ -0,0 +1,12 @@
+package com.example.matheus.mvenosalib
+
+import android.util.Log
+
+/**
+ * Created by appsimples on 22/08/18.
+ */
+class Utils {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+}
